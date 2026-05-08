293362f077d66067f10f5536451f9ae40d7c4755
carlap-dev <carla.dev@gmail.com>
2026-05-08 17:17:43 -0400

    Feat: test al services

---
+++ src/test/java/cl/usm/calculadoraspring/services/CalculadoraServiceTest.java
@@ -0,0 +1,57 @@
+package cl.usm.calculadoraspring.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraServiceTest {
+
+    CalculadoraService calculadoraService;
+
+    @BeforeEach
+    void setUp() {
+        calculadoraService = new CalculadoraService();
+    }
+
+    @Test
+    void calcularSumaOk() {
+        double res = calculadoraService.calcular("+", 1, 2);
+        assertEquals(3, res);
+    }
+
+    @Test
+    void calcularRestaOk() {
+        double res = calculadoraService.calcular("-", 4, 1);
+        assertEquals(3, res);
+    }
+
+    @Test
+    void calcularMultiplicacionOk() {
+        double res = calculadoraService.calcular("*", 3, 1);
+        assertEquals(3, res);
+    }
+
+    @Test
+    void calcularDivisionOk() {
+        double res = calculadoraService.calcular("/", 3, 1);
+        assertEquals(3, res);
+    }
+
+    @Test
+    void calcularDivisionNok() {
+        Exception ex = assertThrows(NumberFormatException.class, ()->{
+            double res = calculadoraService.calcular("/", 2, 0);
+        });
+        assertEquals("can't divide by zero", ex.getMessage());
+    }
+
+    @Test
+    void calcularOperacionNok() {
+        Exception ex = assertThrows(NumberFormatException.class, ()->{
+            double res = calculadoraService.calcular("fake", 2, 0);
+        });
+        assertEquals("Invalid operation", ex.getMessage());
+    }
+
+}
